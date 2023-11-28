1ed79890e8a49dcb9294e55b3ea698b4a3e3aca5
user <huseynovbabek1@gmail.com>
2023-11-28 19:11:49 +0400

    Add project files.

---
+++ Adapter Design Pattern/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Http.Headers;
+
+interface IAdvancedImageViewer
+{
+    void ShowPng();
+    void ShowJpg();
+
+}
+
+
+class PngShower : IAdvancedImageViewer
+{
+    public void ShowJpg()
+    {
+        // code
+    }
+
+    public void ShowPng()
+    {
+        // code
+
+    }
+}
+
+
+class JpgShower : IAdvancedImageViewer
+{
+    public void ShowJpg()
+    {
+        // code
+    }
+
+    public void ShowPng()
+    {
+        // code
+
+    }
+}
+
+
+interface IImageViewer
+{
+    void Show();
+}
+
+class ImageAdapter : IImageViewer
+{
+    private IAdvancedImageViewer AdvanceImageViewer;
+    public ImageAdapter()
+    {
+        
+    }
+    public void Show()
+    {
+        Console.WriteLine("code");
+    }
+}
+
+
+class GalleryApp : IImageViewer
+{
+
+    private ImageAdapter ımageAdapter;
+    public void Show()
+    {
+        //codes
+    }
+}
+
+
+
